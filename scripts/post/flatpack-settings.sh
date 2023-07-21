33e07a132000bac55e8820f8be538c2b3dafacdb
TRS-SoD <TRS-SoD@users.noreply.github.com>
2023-07-21 14:54:31 +0300

    Cleaning up and fixes.

--- scripts/post/flatpack-settings.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -oue pipefail
-
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
-flatpak remote-add --if-not-exists kdeapps-nightly --from https://distribute.kde.org/kdeapps.flatpakrepo
-flatpak remote-add --if-not-exists gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
-flatpak remote-add --if-not-exists elemenetoryos-appcenter https://flatpak.elementary.io/repo.flatpakrepo
