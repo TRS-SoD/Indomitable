74f57b42968983a873dbe5be134bb1419d0be227
TRS-SoD <TRS-SoD@users.noreply.github.com>
2023-07-21 13:01:42 +0300

    Onem ore try to see why sddm isn't running.

--- scripts/post/sddm-theme-config.sh
+++ scripts/post/sddm-theme-config.sh
@@ -6,6 +6,8 @@ systemctl enable sddm.service
 
 systemctl set-default graphical.target
 
+echo "QML_DISABLE_DISK_CACHE=1" >> /etc/sysconfig/sddm
+
 #systemctl isolate graphical.target
 
 #cp /usr/share/sddm/themes/sugar-dark/theme.conf /usr/etc/sddm.conf.d/theme.conf
