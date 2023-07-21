4b6b33a678f4060a083fa7d8bcadf2b170ab6d34
TRS-SoD <TRS-SoD@users.noreply.github.com>
2023-07-21 13:01:42 +0300

    Onem ore try to see why sddm isn't running.

--- scripts/post/sddm-theme-config.sh
+++ scripts/post/sddm-theme-config.sh
@@ -2,20 +2,7 @@
 
 set -oue pipefail
 
-<<<<<<< HEAD
-#systemctl enable sddm
-=======
-systemctl enable sddm.service
->>>>>>> 69f0e63 (one more try)
+systemctl enable sddm
+systemctl set-default graphical.target
 
-#systemctl set-default graphical.target
-
-<<<<<<< HEAD
 cp /usr/share/sddm/themes/sugar-dark/theme.conf /etc/sddm.conf.d/theme.conf
-=======
-#systemctl isolate graphical.target
-
-#cp /usr/share/sddm/themes/sugar-dark/theme.conf /usr/etc/sddm.conf.d/theme.conf
-
-#echo -e "[Theme]\nCurrent=sugar-dark" > /usr/etc/sddm.conf.d/current.conf
->>>>>>> 69f0e63 (one more try)
