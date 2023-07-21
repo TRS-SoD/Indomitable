7fdb4877b50a2d8f2b45813c50f5e9348ae9498f
TRS-SoD <TRS-SoD@users.noreply.github.com>
2023-07-21 08:04:33 +0300

    Fixed sddm?

--- scripts/post/sddm-theme-config.sh
+++ scripts/post/sddm-theme-config.sh
@@ -2,6 +2,10 @@
 
 set -oue pipefail
 
+systemctl enable sddm
+
+systemctl set-default graphical.target
+
 #cp /usr/share/sddm/themes/sugar-dark/theme.conf /usr/etc/sddm.conf.d/theme.conf
 
 #echo -e "[Theme]\nCurrent=sugar-dark" > /usr/etc/sddm.conf.d/current.conf
