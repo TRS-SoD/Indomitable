0d5892c317a5baab52aeaaaefd095a39fc45ddad
TRS-SoD <TRS-SoD@users.noreply.github.com>
2023-07-21 11:06:48 +0300

    one more try

--- scripts/post/sddm-theme-config.sh
+++ scripts/post/sddm-theme-config.sh
@@ -2,8 +2,20 @@
 
 set -oue pipefail
 
+<<<<<<< HEAD
 #systemctl enable sddm
+=======
+systemctl enable sddm.service
+>>>>>>> 69f0e63 (one more try)
 
 #systemctl set-default graphical.target
 
+<<<<<<< HEAD
 cp /usr/share/sddm/themes/sugar-dark/theme.conf /etc/sddm.conf.d/theme.conf
+=======
+#systemctl isolate graphical.target
+
+#cp /usr/share/sddm/themes/sugar-dark/theme.conf /usr/etc/sddm.conf.d/theme.conf
+
+#echo -e "[Theme]\nCurrent=sugar-dark" > /usr/etc/sddm.conf.d/current.conf
+>>>>>>> 69f0e63 (one more try)
