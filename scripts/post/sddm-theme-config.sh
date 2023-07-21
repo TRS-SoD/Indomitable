d1633872474e1c345e7b6eb0a0be3184609e7fd4
TRS-SoD <TRS-SoD@users.noreply.github.com>
2023-07-21 13:38:24 +0300

    fixies

--- scripts/post/sddm-theme-config.sh
+++ scripts/post/sddm-theme-config.sh
@@ -8,9 +8,11 @@ systemctl set-default graphical.target
 
 echo "QML_DISABLE_DISK_CACHE=1" >> /etc/sysconfig/sddm
 
+mkdir -p /usr/etc/sddm.conf.d/
 echo -e "[General]\nDisplayServer=wayland\nGreeterEnvironment=QT_WAYLAND_SHELL_INTEGRATION=layer-shell\n[Wayland]CompositorCommand=" > /usr/etc/sddm.conf.d/current.conf
 
+echo -e "[General]\nDisplayServer=wayland\nGreeterEnvironment=QT_WAYLAND_SHELL_INTEGRATION=layer-shell\n[Wayland]CompositorCommand=" > /etc/sddm.conf.d/current.conf
 
-#cp /usr/share/sddm/themes/sugar-dark/theme.conf /usr/etc/sddm.conf.d/theme.conf
+#cp /usr/share/sddm/themes/sugar-dark/theme.conf /etc/sddm.conf.d/theme.conf
 
-#echo -e "[Theme]\nCurrent=sugar-dark" > /usr/etc/sddm.conf.d/current_theme.conf
+#echo -e "[Theme]\nCurrent=sugar-dark" > /etc/sddm.conf.d/current_theme.conf
