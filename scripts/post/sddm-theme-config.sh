442f70a93b78859357db06c7841744bf2278da48
TRS-SoD <TRS-SoD@users.noreply.github.com>
2023-07-21 13:28:37 +0300

    Sddm wayland generic with settings!

--- scripts/post/sddm-theme-config.sh
+++ scripts/post/sddm-theme-config.sh
@@ -8,8 +8,9 @@ systemctl set-default graphical.target
 
 echo "QML_DISABLE_DISK_CACHE=1" >> /etc/sysconfig/sddm
 
-#systemctl isolate graphical.target
+echo -e "[General]\nDisplayServer=wayland\nGreeterEnvironment=QT_WAYLAND_SHELL_INTEGRATION=layer-shell\n[Wayland]CompositorCommand=" > /usr/etc/sddm.conf.d/current.conf
+
 
 #cp /usr/share/sddm/themes/sugar-dark/theme.conf /usr/etc/sddm.conf.d/theme.conf
 
-#echo -e "[Theme]\nCurrent=sugar-dark" > /usr/etc/sddm.conf.d/current.conf
+#echo -e "[Theme]\nCurrent=sugar-dark" > /usr/etc/sddm.conf.d/current_theme.conf
