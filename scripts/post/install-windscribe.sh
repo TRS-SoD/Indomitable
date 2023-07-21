87cfb34b0f666dfb19f3e48d82fbbc5cdfa676a9
TRS-SoD <TRS-SoD@users.noreply.github.com>
2023-07-21 14:25:10 +0300

    Last commit?

--- scripts/post/install-windscribe.sh
+++ scripts/post/install-windscribe.sh
@@ -2,4 +2,4 @@
 
 set -oue pipefail
 
-#rpm-ostree install https://deploy.totallyacdn.com/desktop-apps/2.6.14/windscribe_2.6.14_x86_64.rpm
+rpm-ostree install https://deploy.totallyacdn.com/desktop-apps/2.6.14/windscribe_2.6.14_x86_64.rpm
