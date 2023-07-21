d1a837cab97e5216b38866608fc0be725bf9f930
TRS-SoD <TRS-SoD@users.noreply.github.com>
2023-07-21 14:30:53 +0300

    Some cleanup and gave up on vpn

--- scripts/post/sddm-theme-config.sh
+++ scripts/post/sddm-theme-config.sh
@@ -6,11 +6,6 @@ systemctl enable sddm.service
 
 systemctl set-default graphical.target
 
-#echo "QML_DISABLE_DISK_CACHE=1" >> /etc/sysconfig/sddm
-
-#mkdir -p /usr/etc/sddm.conf.d/
-#echo -e "[General]\nDisplayServer=wayland\nGreeterEnvironment=QT_WAYLAND_SHELL_INTEGRATION=layer-shell\n[Wayland]CompositorCommand=" > /usr/etc/sddm.conf.d/current.conf
-
 echo -e "[General]\nDisplayServer=wayland\n\n[Wayland]\nCompositorCommand=" > /etc/sddm.conf.d/current.conf
 
 cp /usr/share/sddm/themes/sugar-dark/theme.conf /etc/sddm.conf.d/theme.conf
