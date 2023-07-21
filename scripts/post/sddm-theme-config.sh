6c19705ecc09ea9a5664111f17a008cb33fa1e40
TRS-SoD <TRS-SoD@users.noreply.github.com>
2023-07-21 15:18:17 +0300

    Added pcmanfm to test - fixed some bugs

--- scripts/post/sddm-theme-config.sh
+++ scripts/post/sddm-theme-config.sh
@@ -6,7 +6,4 @@ systemctl enable sddm
 
 systemctl set-default graphical.target
 
-echo -e "[General]\nDisplayServer=wayland\n\n[Wayland]\nCompositorCommand=" > /etc/sddm.conf.d/current.conf
-
 cp /usr/share/sddm/themes/sugar-dark/theme.conf /etc/sddm.conf.d/theme.conf
-echo -e "\n[Theme]\nCurrent=sugar-dark" >> /etc/sddm.conf.d/current.conf
